6a9ce23c02a1ecfa0831b4283936df63b3079a21
Timothy Chelelgo <52329172+akhlys007@users.noreply.github.com>
2025-02-16 12:59:12 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_Nwq8vLHrC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2lhvcs8a3tjf8pbknpno7ebab8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_dg48uESpf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '27uhvkomq3sq70qdr1glu6gps2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
